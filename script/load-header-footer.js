b9fff38ebe3af6c4ea2fb6bed9b4c9f0293d3b62
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2023-12-18 22:58:12 +0100

    auto push

--- script/load-header-footer.js
+++ script/load-header-footer.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Load the header
-    fetch('bid.com/templates/header.html')
+    fetch('/templates/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
         });
 
     // Load the footer
-    fetch('bid.com/templates/footer.html')
+    fetch('/templates/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-container').innerHTML = data;
