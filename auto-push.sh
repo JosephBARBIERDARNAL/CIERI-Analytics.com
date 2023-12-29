8b9cb285431b5100f4452b9ba6007b38c1f07c0f
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2023-12-29 13:56:51 +0100

    some

--- auto-push.sh
+++ auto-push.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Check if a commit message is provided as an argument
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <commit_message>"
+  exit 1
+fi
+
+commit_message="$1"
+
 git add .
-git commit -m "auto push"
-git push origin main
+git commit -m "$commit_message"
+git push 
--- run-server.sh
+++ run-server.sh
@@ -1,1 +1,3 @@
+open http://localhost:8000
 python -m http.server
+
